ce68127414f210f761631848094c634e9fdc2f78
shaay98 <nrichardson567@icloud.com>
2024-12-18 03:33:05 +0000

    practice

---
+++ main.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+let temperature = prompt("What is the temperature?");
+temperature = Fahrenheit("temperature");
+alert("temperature = fahrenheit + celsius");
+document.write
+
+
+let age = prompt("What is your age");
+age = Number;
+if age <= 17 
+alert ("You are under age")
+
+else age >= 18 && 30
+alert ("You are an adult")
+
+else age >= 31 
+alert ("You are a senior citizen")
+
+console.log("Welcome to site")
+
