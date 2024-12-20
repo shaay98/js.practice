f2849c240022ff326b52d576c586272fce1e1d18
shaay98 <nrichardson567@icloud.com>
2024-12-20 03:40:27 +0000

    practice

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ let temperature = prompt("What is the temperature?");
 let f = (temperature * 9) / 5 + 32;
 alert(`temperature is ${f}`);
 
-
 let age = prompt("What is your age");
 age = Number(age);
 if (age <= 17) {
@@ -16,3 +15,22 @@ if (age <= 17) {
 }
 
 console.log("Welcome to site");
+
+let firstuserinput = prompt("enter number here");
+let seconduserinput = prompt("enter number here");
+let operation = prompt("operation =");
+if (operation === "-") {
+  let result = Number(firstuserinput) - Number(seconduserinput);
+  alert("subtract" + result);
+} else if (operation === "+") {
+  let result = Number(firstuserinput) + Number(seconduserinput);
+  alert("add" + result);
+} else if (operation === "/") {
+  let result = Number(firstuserinput) + Number(seconduserinput);
+  alert("divide" + result);
+} else if (operation === "*") {
+  let result = Number(firstuserinput) + Number(seconduserinput);
+  alert("multiply" + result);
+} else {
+  alert("invalid response");
+}
